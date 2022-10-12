2deb228141238aa29f49342de911a011ac118dc1
Usikhumbulele Mlonzi <99178731+UMlonzi@users.noreply.github.com>
2022-10-12 18:26:22 +0200

    sapush

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './data.css'
+import video from './images/1_UINAsA-ZAG8UBYKhENkWiw.gif'
+
 
 function Data() {
   return (
@@ -8,6 +10,12 @@ function Data() {
             <h4>THE LEADING EQUITY PLANNING PLATFORM</h4>
             <h1>You've got startup equity. Now what?</h1>
             <p>see your full net worth including your stock options, compare exercise and selling strategies, and see how taxes impact your net gains</p>
+        </div>
+        <div className='img2'>
+        <img src={video} alt='video' height={300}/>
+
+        {/* <video src={video} width="750" height="500" controls>
+        </video> */}
         </div>
 
     </div>
