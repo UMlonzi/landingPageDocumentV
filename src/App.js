937c8434158e7c0a06b714efb812d12b46feceb1
Usikhumbulele Mlonzi <99178731+UMlonzi@users.noreply.github.com>
2022-10-12 12:35:57 +0200

    push

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import logo from './images/Black-removebg-preview.png'
+import {  Search } from '@rsuite/icons';
 import './navbar.css'
+import { IconButton } from "rsuite";
+
 
 const Navbar = () => {
     const [click, setClick] = useState(false)
+    const ButtonStyle = { margin: "0px 10px" };
 
     const handleClick = () => setClick(!click)
 
@@ -19,6 +23,21 @@ const Navbar = () => {
                  : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
             </div>
             <ul className={click ? "nav-menu active" : "nav-menu"}>
+                 {/* <a href="#">Learn</a>
+            <ul class="dropdown">
+                <li><a href="#">C++</a></li>
+                <li><a href="#">JAVA</a></li>
+                <li><a href="#">PHP</a></li>
+            </ul> */}
+            <div class="dropdown">
+
+              <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+  </div>
                 <li className='nav-item'>
                     <a href='/'>Learn</a>
                 </li>
@@ -34,8 +53,20 @@ const Navbar = () => {
                 <li className='nav-item'>
                     <a href='/'>About</a>
                 </li>
+                <li>
+            {/* <a href="#">Programming languages</a>
+            <ul class="dropdown">
+                <li><a href="#">C++</a></li>
+                <li><a href="#">JAVA</a></li>
+                <li><a href="#">PHP</a></li>
+            </ul> */}
+        </li>
             </ul>
-
+            <button href='/' className='button2'>Log in</button>
+            <button href='/' className='button1'>Get started</button>
+            <div>
+            <IconButton icon={<Search />} className='ButtonStyle' />
+            </div>
         </nav>
         
     </div>
