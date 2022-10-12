a8a34b4daa98888ef90084fa6b9eaaa329a39489
Usikhumbulele Mlonzi <99178731+UMlonzi@users.noreply.github.com>
2022-10-12 16:25:50 +0200

    push

---
+++ src/components/Data.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './data.css'
+
+function Data() {
+  return (
+    <div className='toppest'>
+        <div className='information'>
+            <h4>THE LEADING EQUITY PLANNING PLATFORM</h4>
+            <h1>You've got startup equity. Now what?</h1>
+            <p>see your full net worth including your stock options, compare exercise and selling strategies, and see how taxes impact your net gains</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Data
