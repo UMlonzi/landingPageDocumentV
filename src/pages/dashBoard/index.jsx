89c783c8d06ad7dfc4eb5d87eadf27163ca45e08
Monnus <69508263+Monnus@users.noreply.github.com>
2022-10-19 16:24:39 +0200

    dashboard updates

--- src/pages/dashBoard/index.jsx
+++ src/pages/dashBoard/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState, useEffect}from 'react'
 import Tablecomponent from '../../component/upload/uplaodload'
 // import Navbar from '../../components/navbar/Navbar'
 import { WelcomeCard,UplaodToBlockchainBoxes,DisplayUplaodTable,SelectBox} from "../../component/upload/uplaodload"
@@ -6,13 +6,40 @@ import "./index.css";
 import IconUser from "../../imagess/userIconImage.svg";
 import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
+import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
+import GridViewIcon from '@mui/icons-material/GridView';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import LogoutIcon from '@mui/icons-material/Logout';
 import SearchIcon from '@mui/icons-material/Search';
-import welcomeIMG from "../../imagess/welcomeIMG.svg"
+import welcomeIMG from "../../imagess/welcomeIMG.svg";
+
 import Web from 'web3';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+
 
 const DashBoardPage = () => {
+const [loggInState,setloggInState]=useState(false);
+  const auth=getAuth();
+ useEffect(() => {
+  onAuthStateChanged(auth,(user)=>{
+    if(user){
+      setloggInState(true);
+return;
+    }else if(loggInState==false)return  navigate("/");
+  });
+
+
+ },[loggInState])
+
+  const navigate=useNavigate();
+const handleLogOut=()=>{
+  signOut(auth);
+  setloggInState(false)
+  navigate("/")
+}
+
   return (
 
     <div className="pageGrid" style={{width:`100vw`,height:"100vh"}}>
@@ -26,15 +53,22 @@ const DashBoardPage = () => {
 
 <br/>
   <ul  className='navLI'
-  style={{display:"flex", flexDirection:"column",justifyContent:"space-between",textAlign:"center",width:'100%',height:"20rem"}}>
-    <li><HomeIcon fontSize="large"/>Home</li>
-    <li><InfoIcon fontSize="large"/> About</li>
-    <li> <AccountBoxIcon fontSize='large'/>Profile</li>
+  style={{display:"flex", flexDirection:"column",width:'100%',
+  height:"35rem"}}>
+  <Link to="/">  <li><HomeIcon fontSize="large"/>Home</li></Link>
+  <Link to="/About"> <li><InfoIcon fontSize="large"/> About</li></Link>
+  <Link to="/Dashboard"> <li><GridViewIcon fontSize="large"/> Dashboard</li></Link>
+  <Link to="/profile"> <li> <AccountBoxIcon fontSize='large'/>Profile</li></Link>
+  <Link to="/Plan"> <li> <AirplanemodeActiveIcon fontSize='large'/>Plan</li></Link>
+  <Link to="/ContactUs"> <li> <ContactPhoneIcon fontSize='large'/>Contact Us</li></Link>
+
+
   </ul>
 <br/>
 <hr/>
 <br/>
-<li style={{width:"10rem", height:"5rem",display:"flex", justifyContent:"space-evenly", padding:"2rem 0"}}><LogoutIcon fontSize='large'/>Log out</li>
+<li style={{width:"10rem", height:"5rem",display:"flex", justifyContent:"space-evenly", padding:"2rem 0"}} onClick={handleLogOut}>
+  <LogoutIcon fontSize='large'/>Log out</li>
     </ul>
 
    </nav>
@@ -46,7 +80,7 @@ const DashBoardPage = () => {
 
    
       <div style={{flexGrow:"2",color:"rgba(121,50,128)"}}>
-      <h3>Hello Maonnus</h3>
+      <h3>Hello Monnus</h3>
       <h6>Today is Monday, 20 October 2022</h6>
       </div>
       <div style={{display:"flex",flexGrow:"3",padding:"0 0",textAlign:"center"}}>
